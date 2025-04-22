0db20fb845ec03eef5c018d9b0540cfd37487841
Yuri <yuri.hdl.tonn@gmail.com>
2025-04-21 21:20:43 -0300

    Update Main.java

--- Oficina/src/coisas/Main.java
+++ Oficina/src/coisas/Main.java
@@ -14,13 +14,27 @@ public class Main {
 
 		List<Produto> produtos = new ArrayList<>();
 		Produto p = new Produto();
-		
+
 		System.out.println("=====================================================================");
 		System.out.println("         BEM VINDO AO SISTEMA DE GESTÃO DE PRODUTOS!!");
 		System.out.println("=====================================================================");
 		System.out.println("     Vamos adicionar os produtos");
-		System.out.print("Quantos produtos diferentes serão adicionados: ");
-		tamanho = sc.nextInt();
+		boolean Valido = false;
+		while (!Valido) {
+			try {
+				System.out.print("Quantos produtos diferentes serão adicionados: ");
+				tamanho = sc.nextInt();
+				if (tamanho <= 0) {
+					System.err.println("Deve ser um número maior que 0");
+				} else {
+					Valido = true;
+				}
+			} catch (Exception e) {
+				sc.nextLine();
+				System.err.println("Deve ser um número inteiro");
+			}
+		}
+
 		for (int i = 0; i < tamanho; i++) {
 			Produto produto = new Produto();
 			System.out.println(" " + (i + 1) + "° Produto:");
@@ -30,8 +44,6 @@ public class Main {
 		}
 		System.out.println("Seu(s) produto(s):");
 		p.MostrarProdutos(produtos);
-
-		
 
 		while (cond == true) {
 			System.out.println("\n=================================");
@@ -48,8 +60,16 @@ public class Main {
 
 			switch (opcao) {
 			case 1:
-				System.out.print("Quantos produtos serão adicionados? : ");
-				tamanho = sc.nextInt();
+				while (!Valido) {
+					try {
+						System.out.print("Quantos produtos diferentes serão adicionados: ");
+						tamanho = sc.nextInt();
+						Valido = true;
+					} catch (Exception e) {
+						sc.nextLine();
+						System.err.println("Deve ser um número inteiro");
+					}
+				}
 				for (int i = 0; i < tamanho; i++) {
 					System.out.println("Produto " + (i + 1) + "/" + tamanho);
 					Produto produto = new Produto();
@@ -69,11 +89,10 @@ public class Main {
 				int ide = sc.nextInt();
 				boolean encontrado = false;
 				for (Produto produt : produtos) {
-					
 					if (ide == produt.getId()) {
-				 System.out.println("O que você gostaria de editar?");
-			     System.out.print("[1]-Nome\r[2]-Quantidade\r[3]-Preço\r[4]-validade\r[5]-Tudo\r Opção >> ");
-				 int resp = sc.nextInt();
+						System.out.println("O que você gostaria de editar?");
+						System.out.print("[1]-Nome\r[2]-Quantidade\r[3]-Preço\r[4]-validade\r[5]-Tudo\r Opção >> ");
+						int resp = sc.nextInt();
 						produt.Editar(resp);
 						encontrado = true;
 					}
@@ -88,8 +107,10 @@ public class Main {
 				for (Produto produto : produtos) {
 					System.out.println("Id: " + produto.getId() + ", Nome: " + produto.getNome());
 				}
-				System.out.print(">> ");
-				int ids = sc.nextInt();
+
+				try {
+					System.out.print(">> ");
+				int	ids = sc.nextInt();
 				encontrado = false;
 				for (Produto produto : produtos) {
 					if (ids == produto.getId()) {
@@ -100,10 +121,14 @@ public class Main {
 							produtos.remove(produto.getId());
 						}
 					}
+					if (!encontrado) {
+						System.out.println("Produto não encontrado");
+					}
 				}
-				if (!encontrado) {
-					System.out.println("Produto não encontrado");
+				} catch (Exception e) {
+					System.out.println("Deve ser um número inteiro");
 				}
+
 
 				break;
 			case 4:
@@ -114,9 +139,14 @@ public class Main {
 				break;
 			case 5:
 				System.out.println("\nOrdenar por:\r[1]-Id\r[2]-Nome\r[3]-Estoque\r[4]-Preço\r[5]-Validade");
-				System.out.print(" Opção >> ");
-				int r = sc.nextInt();
-				p.Ordenar(r,produtos);
+				try {
+					System.out.print(" Opção >> ");
+					int r = sc.nextInt();
+					p.Ordenar(r, produtos);
+				} catch (Exception e) {
+					System.out.println("Deve ser um número inteiro");
+				}
+
 				break;
 			case 6:
 				cond = false;
