9bb799132a3646afe87b4ef58ef870b11eb1ff07
Yuri <yuri.hdl.tonn@gmail.com>
2025-05-06 19:36:42 -0300

    Update Produto.java

--- Oficina/src/coisas/Produto.java
+++ Oficina/src/coisas/Produto.java
@@ -1,225 +1,166 @@
 package coisas;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Produto {
+public class Main {
 
-	Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int tamanho = 0;
+		boolean cond = true;
+		int id = 0;
 
-	private int id = 0;
-	private String Nome;
-	private int Quantidade;
-	private double Preco;
+		List<Produto> produtos = new ArrayList<>();
+		Produto p = new Produto();
 
-	private DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-	private String Entrada;
-	private LocalDate Validade;
-
-	public void Preenchimento() {
-
-		System.out.print("\nNome do produto         : ");
-		this.Nome = sc.nextLine();
-
+		System.out.println("=====================================================================");
+		System.out.println("         BEM VINDO AO SISTEMA DE GESTÃO DE PRODUTOS!!");
+		System.out.println("=====================================================================");
+		System.out.println("     Vamos adicionar os produtos");
 		boolean Valido = false;
 		while (!Valido) {
 			try {
-				System.out.print("\nQuantidade de estoque   : ");
-				this.Quantidade = sc.nextInt();
-				if (Quantidade <= 0) {
-					System.err.println("Estoque deve ser maior que 0");
+				System.out.print("Quantos produtos diferentes serão adicionados: ");
+				tamanho = sc.nextInt();
+				if (tamanho <= 0) {
+					System.err.println("Deve ser um número maior que 0");
 				} else {
 					Valido = true;
 				}
 			} catch (Exception e) {
 				sc.nextLine();
-				System.err.println("Número deve ser inteiro!!");
+				System.err.println("Deve ser um número inteiro");
 			}
 		}
 
-		Valido = false;
-		while (!Valido) {
-			try {
-				sc.nextLine();
-				System.out.print("\nPreço do produto      : R$");
-				this.Preco = sc.nextDouble();
-				if (this.Preco <= 0) {
-					System.err.println("Preço deve ser maior que 0");
-				} else {
-					Valido = true;
-				}
-			} catch (Exception e) {
-				System.err.println("Preço deve utilizar ',' ");
-			}
+		for (int i = 0; i < tamanho; i++) {
+			Produto produto = new Produto();
+			System.out.println("\n " + (i + 1) + "° Produto:");
+			produto.setId(id++);
+			produto.Preenchimento();
+			produtos.add(produto);
 		}
+		System.out.println("Seu(s) produto(s):");
+		p.MostrarProdutos(produtos);
 
-		Valido = false;
-		while (!Valido) {
-			try {
-				sc.nextLine();
-				System.out.print("\nValidade do produto(dd/mm/aaaa)   : ");
-				setEntrada(sc.next());
-				Valido = true;
+		while (cond == true) {
+			System.out.println("\n=================================");
+			System.out.println("              MENU");
+			System.out.println("=================================\n");
+			System.out.println("[1]- Adicionar produto");
+			System.out.println("[2]- Editar produto");
+			System.out.println("[3]- Remover produto");
+			System.out.println("[4]- Visualizar produtos");
+			System.out.println("[5]- Ordernar produtos");
+			System.out.println("[6]- Sair");
+			System.out.print(" Opção >> ");
+			int opcao = sc.nextInt();
 
-			} catch (Exception e) {
-				System.err.println("Data no formato errado!");
-			}
-		}
-	}
-
-	public void Editar(int resp) {
-		switch (resp) {
-		case 1:
-			System.out.print("Novo nome de " + getNome() + ": ");
-			this.Nome = sc.nextLine();
-			break;
-		case 2:
-			boolean Valido = false;
-			try {
-				System.out.print("\nNova quantidade de estoque " + getNome() + ": ");
-				this.Quantidade = sc.nextInt();
-				if (Quantidade <= 0) {
-					System.err.println("Estoque deve ser maior que 0");
-				} else {
-					Valido = true;
-				}
-			} catch (Exception e) {
-				sc.nextLine();
-				System.err.println("Número deve ser inteiro!!");
-			}
-			break;
-		case 3:
-			Valido = false;
-			while (!Valido) {
-				try {
-					sc.nextLine();
-					System.out.print("\nNovo Preço de " + getNome() + " : R$");
-					this.Preco = sc.nextDouble();
-					if (this.Preco <= 0) {
-						System.err.println("Preço deve ser maior que 0");
-					} else {
+			switch (opcao) {
+			case 1:
+				while (!Valido) {
+					try {
+						System.out.print("Quantos produtos diferentes serão adicionados: ");
+						tamanho = sc.nextInt();
 						Valido = true;
+					} catch (Exception e) {
+						sc.nextLine();
+						System.err.println("Deve ser um número inteiro");
+					}
+				}
+				for (int i = 0; i < tamanho; i++) {
+					System.out.println("Produto " + (i + 1) + "/" + tamanho);
+					Produto produto = new Produto();
+					produto.setId(id++);
+					produto.Preenchimento();
+					produtos.add(produto);
+
+				}
+
+				break;
+			case 2:
+				System.out.println("Que produto você deseja editar?(Id)");
+				for (Produto produto : produtos) {
+					System.out.println("Id: " + produto.getId() + ", Nome: " + produto.getNome());
+				}
+				boolean encontrado = false;
+				try {
+					System.out.print(">> ");
+					int ide = sc.nextInt();
+					for (Produto produt : produtos) {
+						if (ide == produt.getId()) {
+							System.out.println("O que você gostaria de editar?");
+							System.out.print("[1]-Nome\r[2]-Quantidade\r[3]-Preço\r[4]-validade\r[5]-Tudo\r Opção >> ");
+							int resp = sc.nextInt();
+							produt.Editar(resp);
+							encontrado = true;
+						}
+					}
+					if (!encontrado) {
+						System.err.println("Produto não encontrado");
+					}
+
+				} catch (Exception e) {
+					System.out.println("Deve ser um Número inteiro");
+				}
+
+				break;
+
+			case 3:
+				System.out.println("Que produto você deseja remover?(Id)");
+				for (Produto produto : produtos) {
+					System.out.println("Id: " + produto.getId() + ", Nome: " + produto.getNome());
+				}
+
+				try {
+					System.out.print(">> ");
+					int ids = sc.nextInt();
+					encontrado = false;
+					for (Produto produto : produtos) {
+						if (ids == produto.getId()) {
+							System.out.println("Tem certeza que deseja remover " + produto.getNome() + "?[sim/não]");
+							encontrado = true;
+							String resp = sc.next().toLowerCase();
+							if (resp.equals("sim")) {
+								produtos.remove(produto.getId());
+							}
+						}
+						if (!encontrado) {
+							System.out.println("Produto não encontrado");
+						}
 					}
 				} catch (Exception e) {
-					System.err.println("Preço deve utilizar ',' ");
+					System.out.println("Deve ser um número inteiro");
 				}
-			}
-			break;
-		case 4:
-			Valido = false;
-			while (!Valido) {
+
+				break;
+			case 4:
+				for (Produto Produtos : produtos) {
+					System.out.println(Produtos.toString());
+				}
+
+				break;
+			case 5:
+				System.out.println("\nOrdenar por:\r[1]-Id\r[2]-Nome\r[3]-Estoque\r[4]-Preço\r[5]-Validade");
 				try {
-					sc.nextLine();
-					System.out.print("Nova Validade " + getNome() + ": ");
-					Valido = true;
-					setEntrada(sc.next());
+					System.out.print(" Opção >> ");
+					int r = sc.nextInt();
+					p.Ordenar(r, produtos);
 				} catch (Exception e) {
-					System.err.println("Data no formato errado!");
+					System.out.println("Deve ser um número inteiro");
 				}
+
+				break;
+			case 6:
+				cond = false;
+				System.out.println("Muito Obrigado!!");
+				break;
+
+			default:
+				break;
 			}
-			break;
-		case 5:
-			Preenchimento();
-			break;
-
-		default:
-			System.out.println("Opção inválida");
-			break;
 		}
-	}
-
-	public void MostrarProdutos(List<Produto> produtos) {
-		for (Produto produto : produtos) {
-			System.out.println(produto);
-		}
-	}
-
-	public void Ordenar(int resp, List<Produto> produtos) {
-		switch (resp) {
-		case 1:
-			produtos.sort(Comparator.comparing(Produto::getId));
-			MostrarProdutos(produtos);
-			break;
-		case 2:
-			produtos.sort(Comparator.comparing(Produto::getNome));
-			MostrarProdutos(produtos);
-			break;
-		case 3:
-			produtos.sort(Comparator.comparing(Produto::getQuantidade));
-			MostrarProdutos(produtos);
-			break;
-		case 4:
-			produtos.sort(Comparator.comparing(Produto::getPreco));
-			MostrarProdutos(produtos);
-			break;
-		case 5:
-			produtos.sort(Comparator.comparing(Produto::getValidadeData));
-			MostrarProdutos(produtos);
-			break;
-		default:
-			System.out.println("Organização cancelada");
-
-			break;
-		}
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return Nome;
-	}
-
-	public void setNome(String nome) {
-		this.Nome = nome;
-	}
-
-	public int getQuantidade() {
-		return Quantidade;
-	}
-
-	public void setQuantidade(int quantidade) {
-		this.Quantidade = quantidade;
-	}
-
-	public double getPreco() {
-		return Preco;
-	}
-
-	public void setPreco(double preco) {
-		this.Preco = preco;
-	}
-
-	public LocalDate getValidadeData() {
-		return Validade;
-	}
-
-	public String getValidade() {
-		String validade = Validade.format(formato);
-		return validade;
-
-	}
-
-	public String getEntrada() {
-		return Entrada;
-	}
-
-	public void setEntrada(String Entrada) {
-		this.Entrada = Entrada;
-		Validade = LocalDate.parse(Entrada, formato);
-	}
-
-	@Override
-	public String toString() {
-		return " Id = " + id + ", Nome= " + Nome + ", Quantidade= " + Quantidade + ", Preco= " + Preco + ", Validade "
-				+ getValidade();
 	}
 }
